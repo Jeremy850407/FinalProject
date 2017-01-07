4771f5877e264cc74914e8a37b0eae08402b1234
user_23 <user_23@mac-23deimac.local>
2017-01-07 13:45:08 +0800

    startup

---
+++ IOSFinal/AccountTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountTableViewCell.swift
+//  IOSFinal
+//
+//  Created by user_23 on 2017/1/7.
+//  Copyright © 2017年 user_23. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
